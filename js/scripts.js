2fc61f5ca1e2ebe81894b3e7fb418667f6cd6f88
Gabr19 <sannagabriele19@gmail.com>
2023-01-11 18:21:20 +0100

    richiamo contenitore

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-const img = ['img1' , 'img2' , 'img3' , 'img4' , 'img5']
-    console.log('img' , img , typeof img)
+// Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+// MILESTONE 1
+// Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+// MILESTONE 2
+// Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
+// Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+// Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+
+const images = ['01.webp' , '02.webp' , '03.webp' , '04.webp' , '05.webp']
+
+    for(let i = 0 ; i < images.length ; i++){
+        let img = `<img id="img1" src="img/${images[i]}">`
+        console.log(img)
+        // richiamare il contenitore
+        document.getElementById('container').innerHTML = img;
+        // inserire img nel contenitore
+
+    }
 
