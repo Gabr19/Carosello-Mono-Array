fd1e35a7ddf2b96bda068ec3003a2523ccaf2a9c
Gabr19 <sannagabriele19@gmail.com>
2023-01-11 18:22:57 +0100

    cambio metodo richiamo contenitore

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,8 @@ const images = ['01.webp' , '02.webp' , '03.webp' , '04.webp' , '05.webp']
         let img = `<img id="img1" src="img/${images[i]}">`
         console.log(img)
         // richiamare il contenitore
-        document.getElementById('container').innerHTML = img;
+        let cont = document.getElementById('container')
+        cont.innerHTML = img;
         // inserire img nel contenitore
 
     }
